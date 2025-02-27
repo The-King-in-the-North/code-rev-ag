de488d56cf2485bbd2125802230953913d2f033b
Abhay Chandramouli <32009094+The-King-in-the-North@users.noreply.github.com>
2025-02-27 10:29:27 +0530

    Add files via upload

---
+++ myagent-test.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace Agent.Abhay
+{
+    public class myagent_test
+    {
+        private readonly ILogger<myagent_test> _logger;
+
+        public myagent_test(ILogger<myagent_test> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("myagent_test")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult("Welcome to Azure Functions!");
+        }
+    }
+}
